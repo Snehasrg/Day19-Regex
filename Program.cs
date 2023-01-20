acdcf5c582ae138185e9a665e495edc57fabc931
Snehasrg <sghongade642@gmail.com>
2023-01-20 10:24:06 +0530

    Method Calling

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the User Regestration Program by Regex");
+            MethodStoring.ForFirstName();
 
         }
 
